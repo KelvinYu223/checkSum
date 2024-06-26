606f4880511617bbe9e007aa190740ab88703708
Yu Han Yuan <hanyuan.yu@meiban.com>
2024-06-26 08:55:39 +0800

    Add project files.

---
+++ checkSum/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Private Sub checkSum()
+        Dim totalInt As Long
+        Dim input As String = TextBox1.Text
+        For Each oneChar As Char In input
+            Dim AscCode As SByte = Asc(oneChar)
+            totalInt = totalInt Xor AscCode
+        Next
+        ListBox1.Items.Add(Hex(totalInt))
+    End Sub
+
+    Private Sub ButtonCs_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        checkSum()
+    End Sub
+End Class
